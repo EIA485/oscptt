02ab95a97e34be91d036db7fbebf33aba8002930
user <user@DESKTOP-CD9F5VF>
2022-06-07 17:17:53 -0700

    Add project files.

---
+++ dcpttosc/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine(@"
+    
+  \   /
+  - | - 
+  / | \
+    |
+    |   __
+  __|___||__
+  | ______ |
+  | |osc | |  
+  | |____| |
+  |        |
+  |        |
+  | .::::. |
+  | :::::: |
+  | `::::` |
+  |________|
+");
+
+
+SimpleOSC simpleOSC = new();
+List<SimpleOSC.OSCMessage> msg = new List<SimpleOSC.OSCMessage>();
+simpleOSC.OpenClient(9001);
+while (true){
+    simpleOSC.GetIncomingOSC(msg);
+    foreach (SimpleOSC.OSCMessage m in msg)
+    {
+        if (m.path == "/avatar/parameters/unmute")
+           SendKeys.SendWait("{PGDN}");
+    }
+    msg.Clear();
+    Thread.Sleep(100);
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+#simple program to press keys from osc data
+
+#credits 
+using (lyuma's simple osc library)[https://gist.github.com/lyuma/120d2736d6963460fc641fe24c1b02f7]
